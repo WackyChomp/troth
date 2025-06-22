73c7707ddb13ab4e4c0e70fa49e51db0dde7f686
Wilson Chan <wilsonchan54@gmail.com>
2025-06-21 22:35:47 -0400

    Rendered mock data from constant index.ts from TrekCard component (WIP) / updated styling in app.css

--- components/TrekCard.tsx
+++ components/TrekCard.tsx
@@ -1,12 +1,30 @@
 import React from 'react'
 import { Link, useLocation } from 'react-router'
 
-const TrekCard = ({ id, name, imageUrls, itinerary, tags, estimatedPrice } : TrekCardProps) => {
+const TrekCard = ({ id, name, imageUrls, location, itinerary, tags, estimatedPrice } : TrekCardProps) => {
   const path = useLocation();
 
   return (
-    <Link to={path.pathname == '/' || path.pathname.startsWith('/travel') ? `/travel/${id}` : `/treks/${id}`}>
+    <Link to={path.pathname == '/' || path.pathname.startsWith('/travel') ? `/travel/${id}` : `/treks/${id}`} className='trek_card'>
       <img src={imageUrls} alt="" />
+
+      <article className='bg-red-900'>
+        <h2>{name}</h2>
+        <figure>
+          <img src="" alt="location" className='size-5' />
+          <figcaption>{location}</figcaption>
+        </figure>
+      </article>
+
+      <div className="mt-5 pl-[20px] pr-4 pb-6">
+        {tags.map((tag, index) => (
+          <div key={index} className="bg-amber-300 flex">{tag}</div>
+        ))}
+      </div>
+
+      <article className='trekCard_pill'>
+        {estimatedPrice}
+      </article>
     </Link>
   )
 }
