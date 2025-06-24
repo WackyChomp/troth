042c5a0f352b399ce19e5eebf44156d254acad03
Wilson Chan <wilsonchan54@gmail.com>
2025-06-23 21:31:51 -0400

    Added getGooglePicture, logUserData, and storeUserData functions in auth.ts

--- app/appwrite/auth.ts
+++ app/appwrite/auth.ts
@@ -1,4 +1,4 @@
-import { OAuthProvider, Query } from "appwrite"
+import { ID, OAuthProvider, Query } from "appwrite"
 import { appwriteConfig, account, database } from "./client"
 import { redirect } from "react-router"
 
@@ -6,7 +6,7 @@ export const loginWithGoogle = async() => {
   try {
     account.createOAuth2Session(OAuthProvider.Google)
   } catch (error) {
-    console.log('Error Origin: createOAuth2Session',error)
+    console.log('loginWithGoogle error: ',error)
   }
 }
 
@@ -25,6 +25,87 @@ export const getUser = async() => {
       ]
     )
   } catch (error) {
-    console.log(error)
+    console.log('getUser error: ', error)
+  }
+}
+
+export const getGooglePicture = async() => {
+  try {
+    const session = await account.getSession('current');
+    const oAuthToken = session.providerAccessToken;
+
+    if(!oAuthToken){
+      console.log('No oAuth token exists');
+      return null;
+    }
+
+    const response = await fetch(
+      'https://people.googleapis.com/v1/people/me?personFields=photos',
+      {
+        headers: {
+          Authorization: `Bearer ${oAuthToken}`
+        }
+      }
+    )
+
+    if(!response.ok){
+      console.log('Failed  to fetch photo from Google People API');
+      return null;
+    }
+
+    const data = await response.json();
+    const photoUrl = data.photos && data.photos.length > 0
+      ? data.photos[0].url
+      : null;
+
+    return photoUrl;
+  } catch (error) {
+    console.log('getGooglePicture error: ', error)
+  }
+}
+
+export const logoutUser = async() => {
+  try {
+    await account.deleteSession('current');
+    return true;
+  } catch (error) {
+    console.log('logoutUser error: ', error)
+  }
+}
+
+export const storeUserData = async() => {
+  try {
+    const user = await account.get();
+    if (!user) return null;
+
+    const { documents } = await database.listDocuments(
+      appwriteConfig.databaseId,
+      appwriteConfig.userCollectionId,
+      [
+        Query.equal('accountId', user.$id)
+      ]
+    );
+
+    if (documents.length > 0) return documents[0];
+
+    const imageUrl = await getGooglePicture();
+
+    const newUser = await database.createDocument(
+      appwriteConfig.databaseId,
+      appwriteConfig.userCollectionId,
+      ID.unique(),
+      {
+        accountId: user.$id,
+        email: user.email,
+        name: user.name,
+        imageUrl: imageUrl || '',
+        joinedAt: new Date().toISOString(),
+      }
+    );
+
+    return newUser;
+
+  } catch (error) {
+    console.log('storeUserData error:', error)
   }
 }
