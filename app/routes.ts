568d5923a553110217456bc3cc4f016ba816ec5a
Wilson Chan <wilsonchan54@gmail.com>
2025-06-24 22:24:59 -0400

    Created sign-in.tsx (WIP) / added sign-in route to route.ts / updated app.css

--- app/routes.ts
+++ app/routes.ts
@@ -2,6 +2,7 @@ import { type RouteConfig, index, layout, route } from "@react-router/dev/routes
 
 export default [
   index("routes/home.tsx"),
+  route('sign-in', 'routes/root/sign-in.tsx'),
   layout('routes/admin/admin-layout.tsx', [
     route('dashboard', 'routes/admin/dashboard.tsx'),
     route('all-users', 'routes/admin/all-users.tsx'),
---
+++ app/routes/root/sign-in.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Link } from 'react-router'
+
+const signIn = () => {
+  return (
+    <main className='auth'>
+      <section className='size-full glassmorphism flex-center px-5'>
+        <div className="signIn_card">
+          <header className='header'>
+            <Link to='/'>
+              <img src="https://img.icons8.com/ios/200/feeding-chicken.png" alt="logo" className='bg-red-400 rounded-full size-[40px]' />
+            </Link>
+            <h1 className='text-3xl font-bold text-red-400 uppercase'>Troth</h1>
+
+            <article className='text-red-400 border-red-400 border-3'>
+              <h2 className='text-xl font-semibold text-center capitalize'>Begin your trek</h2>
+              <p className='text-sm text-center !leading-6'>Sign in with Google to manage your treks and activity seamlessly</p>
+            </article>
+
+            <button className='bg-blue-400'>
+              <img src='https://img.icons8.com/ios/50/google-logo--v1.png' alt="Google Icon" className='size-7' />
+              <span className='text-sm font-semibold'>Sign in with Google</span>
+            </button>
+          </header>
+        </div>
+      </section>
+    </main>
+  )
+}
+
+export default signIn
